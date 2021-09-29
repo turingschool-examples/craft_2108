04be1c296040fb64cc4a6098d37b6f745bc71be3
Chaz Simons <chaz.simons@gmail.com>
2021-09-29 08:04:53 -0700

    Initial commit. First test for person down

---
+++ lib/person.rb
@@ -0,0 +1,7 @@
+class Person
+
+  def initialize(attributes)
+    @name = attributes[:name]
+    @interests = attributes[:interests]
+  end
+end 
---
+++ spec/person_spec.rb
@@ -0,0 +1,11 @@
+require './lib/person'
+
+describe Person do
+  before(:each) do
+    @person = Person.new({name: 'Hector', interests: ['sewing', 'millinery', 'drawing']})
+  end
+
+  it 'exists' do
+    expect(@person).to be_an_instance_of(Person)
+  end
+end
