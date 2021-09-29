c715cea4d4f1d591a6799ada4a6183f3a29eb65e
Alex McConnell <alexmmcconnell@gmail.com>
2021-09-29 10:52:32 -0600

    Test - Add craft related tests

--- spec/event_spec.rb
+++ spec/event_spec.rb
@@ -9,7 +9,7 @@ describe Event do
     @toni = Person.new({name: 'Toni', interests: ['sewing', 'knitting']})
     @zoey = Person.new({name: 'Zoey', interests: ['drawing', 'knitting']})
     @hector = Person.new({name: 'Hector', interests: ['sewing', 'millinery', 'drawing']})
-    @event = Event.new("Carla's Craft Connection", [@sewing, @knitting, @painting], [@carla, @hector, @zoey])
+    @event = Event.new("Carla's Craft Connection", [@sewing, @knitting, @painting], [@toni, @hector, @zoey])
   end
 
   it 'has readable attributes' do
@@ -29,12 +29,56 @@ describe Event do
 
   describe '#supply_list' do
     it 'returns what supplies are required for all given crafts' do
-      expect(@event.supply_list).to eq(["fabric", "scissors", "thread", "sewing_needles", "yarn", "knitting_needles"])
+      expect(@event.supply_list).to include("fabric", "scissors", "thread", "sewing_needles", "yarn", "knitting_needles")
     end
   end
 
   describe '#attendees_by_craft_interest' do
     it 'returns all attendees by craft interest' do
+      expected = {
+        "knitting"=>[@toni, @zoey],
+        "painting"=>[],
+        "sewing"=>[@toni, @hector]
+      }
+      expect(@event.attendees_by_craft_interest).to eq(expected)
+    end
+  end
+
+  describe '#crafts_that_use' do
+    it 'returns all crafts that use a given material' do
+      expect(@event.crafts_that_use('scissors')).to eq([@sewing, @knitting])
+      expect(@event.crafts_that_use('fire')).to eq([])
+    end
+  end
+
+  describe '#assign_attendees_to_crafts' do
+    it 'randomly assigns attendees to crafts' do
+      hector = Person.new({name: 'Hector', interests: ['sewing', 'millinery', 'drawing', 'painting']})
+      toni = Person.new({name: 'Toni', interests: ['sewing', 'knitting']})
+      zoey = Person.new({name: 'Zoey', interests: ['drawing', 'knitting', 'painting']})
+      toni.add_supply('yarn', 30)
+      toni.add_supply('scissors', 2)
+      toni.add_supply('knitting_needles', 5)
+      toni.add_supply('fabric', 10)
+      toni.add_supply('scissors', 1)
+      toni.add_supply('thread', 2)
+      toni.add_supply('paint_brush', 10)
+      toni.add_supply('paints', 20)
+      zoey.add_supply('yarn', 20)
+      zoey.add_supply('scissors', 2)
+      zoey.add_supply('knitting_needles', 2)
+      hector.add_supply('fabric', 5)
+      hector.add_supply('scissors', 1)
+      hector.add_supply('thread', 1)
+      hector.add_supply('canvas', 5)
+      hector.add_supply('paint_brush', 10)
+      hector.add_supply('paints', 20)
+      event = Event.new("Carla's Craft Connection", [@sewing, @knitting, @painting], [toni, hector, zoey])
+
+      expect(event.assign_attendees_to_crafts.values.flatten).to include(toni, hector, zoey)
+      expect(event.assign_attendees_to_crafts[@sewing]).not_to include(zoey)
+      expect(event.assign_attendees_to_crafts[@knitting]).not_to include(hector)
+      expect(event.assign_attendees_to_crafts[@painting]).not_to include(toni)
     end
   end
 end
