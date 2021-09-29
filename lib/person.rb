a37803b8e0912baedcea96fa83e84638a74160de
Chaz Simons <chaz.simons@gmail.com>
2021-09-29 08:07:42 -0700

    Feat: Person now has attributes and tests for them

--- lib/person.rb
+++ lib/person.rb
@@ -1,7 +1,11 @@
 class Person
+  attr_reader :name,
+              :interests,
+              :supplies
 
   def initialize(attributes)
-    @name = attributes[:name]
+    @name      = attributes[:name]
     @interests = attributes[:interests]
+    @supplies  = Hash.new(0)
   end
-end 
+end
--- spec/person_spec.rb
+++ spec/person_spec.rb
@@ -8,4 +8,13 @@ describe Person do
   it 'exists' do
     expect(@person).to be_an_instance_of(Person)
   end
+
+  it 'has attributes' do
+    expect(@person.name).to eq('Hector')
+    expect(@person.interests).to eq(['sewing', 'millinery', 'drawing'])
+  end
+
+  it 'starts with no supplies' do
+    expect(@person.supplies).to eq({})
+  end
 end
