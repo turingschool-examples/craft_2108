d1fe852c1517e42cf368fb0804b8d2595bd717fa
Devin Pile <devinpile@gmail.com>
2021-09-29 10:03:52 -0600

    Add event test for craft most supplies

--- spec/event_spec.rb
+++ spec/event_spec.rb
@@ -10,7 +10,7 @@ describe Event do
       name: 'Hector',
       interests: ['sewing', 'millinery', 'drawing']
     })
-    @event = Event.new("Carla's Craft Connection", @craft, @person)
+    @event = Event.new("Carla's Craft Connection", [@craft], [@person])
   end
 
   it 'exists' do
@@ -19,8 +19,8 @@ describe Event do
 
   it 'has readable attributes' do
     expect(@event.name).to eq("Carla's Craft Connection")
-    expect(@event.crafts).to eq(@craft)
-    expect(@event.attendees).to eq(@person)
+    expect(@event.crafts).to eq([@craft])
+    expect(@event.attendees).to eq([@person])
   end
 
   describe '#attendee_names' do
@@ -40,4 +40,22 @@ describe Event do
       expect(@event.attendee_names).to eq(["Hector", "Toni"])
     end
   end
+
+  describe '#craft_with_most_supplies' do
+    it 'can return the craft with the most supplies' do
+      @knitting = Craft.new('knitting', {yarn: 20, scissors: 1, knitting_needles: 2})
+      @sewing = Craft.new('sewing', {fabric: 5, scissors: 1, thread: 1, sewing_needles: 1})
+      @hector = Person.new({
+        name: 'Hector',
+        interests: ['sewing', 'millinery', 'drawing']
+      })
+      @toni = Person.new({
+        name: 'Toni',
+        interests: ['sewing', 'knitting']
+      })
+      @event = Event.new("Carla's Craft Connection", [@sewing, @knitting], [@hector, @toni])
+
+      expect(@event.craft_with_most_supplies).to eq('sewing')
+    end
+  end 
 end
