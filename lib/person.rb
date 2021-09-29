c0fd062ed7e09ae36e8647912d9f96fcafe0b07a
Paul Leonard <paulpaulleonard@gmail.com>
2021-09-29 12:16:46 -0400

    Iteration 1 minus a perfected add_supply method

--- lib/person.rb
+++ lib/person.rb
@@ -1,12 +1,17 @@
 class Person
-  attr_reader :name,
-              :interests,
-              :supplies
+  attr_reader   :name,
+                :interests
+  attr_accessor :supplies
 
   def initialize(info)
     @name      = info[:name]
     @interests = info[:interests]
     @supplies  = {}
+  end
+
+  def add_supply(type, number_supplied)
+    @supplies[type] = number_supplied
+    #spent too long trying to figure this out and just went ahead
   end
 
 end
