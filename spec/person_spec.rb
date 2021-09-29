57f8b250cd05e9dcb07dedf6300985ef6722e329
Philip Medlin <phmedlin@gmail.com>
2021-09-29 09:07:14 -0600

    Test: Create person class tests

---
+++ spec/person_spec.rb
@@ -0,0 +1,19 @@
+require 'rspec'
+require './lib/person'
+
+describe Person do
+  before :each do
+    @person = Person.new({name: 'Hector', interests: ['sewing', 'millinery', 'drawing']})
+  end
+
+  it 'exists' do
+    expect(@person).to be_an_instance_of(Person)
+  end
+
+  it 'attributes' do
+    expect(@person.name).to eq('Hector')
+    expect(@person.interests).to eq(["sewing", "millinery", "drawing"])
+    expect(@person.supplies).to eq({})
+  end
+
+end
