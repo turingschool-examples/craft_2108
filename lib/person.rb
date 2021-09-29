94ba45c93ed340cdd00e2ab52ab48d34e15383ef
John Napier <jcee.napier@gmail.com>
2021-09-29 10:48:26 -0600

    Add final ic

---
+++ lib/person.rb
@@ -0,0 +1,18 @@
+class Person
+  attr_reader :name,
+              :interests,
+              :supplies
+
+  def initialize(info)
+    @name      = info[:name]
+    @interests = info[:interests]
+    @supplies  = {}
+  end
+
+  def add_supply(item, quantity)
+    @supplies[item] ||= quantity
+    if @supplies[item] != quantity
+      @supplies[item] += quantity
+    end
+  end
+end
