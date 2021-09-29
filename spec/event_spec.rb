f656a61e55aec31107d049d096b3950beaa5b8bf
Sam Devine <samuel.will.devine@gmail.com>
2021-09-29 10:47:27 -0600

    skipping Iteration 4 test, not enough time

--- spec/event_spec.rb
+++ spec/event_spec.rb
@@ -64,4 +64,43 @@ RSpec.describe Event do
       expect(@event.crafts_that_use('fire')).to eq []
     end
   end
+
+  context 'Iteration 4' do
+    xit '#assign_attendees_to_crafts assigns randomly' do
+      hector = Person.new({name: 'Hector', interests: ['sewing', 'millinery', 'drawing', 'painting']})
+      toni = Person.new({name: 'Toni', interests: ['sewing', 'knitting']})
+      zoey = Person.new({name: 'Zoey', interests: ['drawing', 'knitting', 'painting']})
+      knitting = Craft.new('knitting', {yarn: 20, scissors: 1, knitting_needles: 2})
+      sewing = Craft.new('sewing', {fabric: 5, scissors: 1, thread: 1})
+      painting = Craft.new('painting', {canvas: 1, paint_brush: 2, paints: 5})
+
+      toni.add_supply('yarn', 30)
+      toni.add_supply('scissors', 2)
+      toni.add_supply('knitting_needles', 5)
+      toni.add_supply('fabric', 10)
+      toni.add_supply('scissors', 1)
+      toni.add_supply('thread', 2)
+      toni.add_supply('paint_brush', 10)
+      toni.add_supply('paints', 20)
+      zoey.add_supply('yarn', 20)
+      zoey.add_supply('scissors', 2)
+      zoey.add_supply('knitting_needles', 2)
+      hector.add_supply('fabric', 5)
+      hector.add_supply('scissors', 1)
+      hector.add_supply('thread', 1)
+      hector.add_supply('canvas', 5)
+      hector.add_supply('paint_brush', 10)
+      hector.add_supply('paints', 20)
+
+      event = Event.new("Carla's Craft Connection", [knitting, sewing, painting], [hector, toni, zoey])
+
+      assignment1 = event.assign_attendees_to_crafts
+      assignment2 = event.assign_attendees_to_crafts
+      assignment3 = event.assign_attendees_to_crafts
+
+      expect(assignment1 != assignment2).to be true
+      expect(assignment2 != assignment3).to be true
+      expect(assignment1 != assignment3).to be true
+    end
+  end
 end
