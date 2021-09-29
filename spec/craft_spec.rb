454740d090a59122eb123593832400bc159a8882
Alex McConnell <alexmmcconnell@gmail.com>
2021-09-29 10:52:08 -0600

    Test - Add helper method test

--- spec/craft_spec.rb
+++ spec/craft_spec.rb
@@ -10,4 +10,10 @@ describe Craft do
     expect(@knitting.name).to eq('knitting')
     expect(@knitting.supplies_required).to eq({yarn: 20, scissors: 1, knitting_needles: 2})
   end
+
+  describe 'materials_required' do
+    it 'returns all materials required for a given craft' do
+      expect(@knitting.materials_required).to eq([:yarn, :scissors, :knitting_needles])
+    end
+  end
 end
